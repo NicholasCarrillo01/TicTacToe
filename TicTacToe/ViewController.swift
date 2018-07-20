394cba67b11d398436088b57df1667695d9ddb11
Nicholas Carrillo <nicholasmcxbox@gmail.com>
2018-07-20 13:29:12 -0400

    finished tictactoe game

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -34,39 +34,93 @@ class ViewController: UIViewController {
         button1.isEnabled = false
         }
     @IBAction func button2Tapped(_ sender: Any) {
-        print("Button 2 tapped")
+        if isXTurn {
+            button2.setBackgroundImage(#imageLiteral(resourceName: "X"), for: .normal)
+        } else {
+            button2.setBackgroundImage(#imageLiteral(resourceName: "O"), for: .normal)
+        }
+        //set whose turn it is to the opposite
+        isXTurn = !isXTurn
+        button2.isEnabled = false
     }
     
     @IBAction func button3Tapped(_ sender: Any) {
-        print("Button 3 tapped")
+        if isXTurn {
+            button3.setBackgroundImage(#imageLiteral(resourceName: "X"), for: .normal)
+        } else {
+            button3.setBackgroundImage(#imageLiteral(resourceName: "O"), for: .normal)
+        }
+        //set whose turn it is to the opposite
+        isXTurn = !isXTurn
+        button3.isEnabled = false
     }
     
     @IBAction func button4Tapped(_ sender: Any) {
-        print("Button 4 tapped")
+        if isXTurn {
+            button4.setBackgroundImage(#imageLiteral(resourceName: "X"), for: .normal)
+        } else {
+            button4.setBackgroundImage(#imageLiteral(resourceName: "O"), for: .normal)
+        }
+        //set whose turn it is to the opposite
+        isXTurn = !isXTurn
+        button4.isEnabled = false
     }
     @IBAction func button5Tapped(_ sender: Any) {
-        print("Button 5 tapped")
+        if isXTurn {
+            button5.setBackgroundImage(#imageLiteral(resourceName: "X"), for: .normal)
+        } else {
+            button5.setBackgroundImage(#imageLiteral(resourceName: "O"), for: .normal)
+        }
+        //set whose turn it is to the opposite
+        isXTurn = !isXTurn
+        button5.isEnabled = false
+        
     }
+    
     @IBAction func button6Tapped(_ sender: Any) {
-        print("Button 6 tapped")
+        if isXTurn {
+            button6.setBackgroundImage(#imageLiteral(resourceName: "X"), for: .normal)
+        } else {
+            button6.setBackgroundImage(#imageLiteral(resourceName: "O"), for: .normal)
+        }
+        //set whose turn it is to the opposite
+        isXTurn = !isXTurn
+        button6.isEnabled = false
+        
     }
     @IBAction func button7tapped(_ sender: Any) {
-        print("Button 7 tapped")
+        if isXTurn {
+            button7.setBackgroundImage(#imageLiteral(resourceName: "X"), for: .normal)
+        } else {
+            button7.setBackgroundImage(#imageLiteral(resourceName: "O"), for: .normal)
+        }
+        //set whose turn it is to the opposite
+        isXTurn = !isXTurn
+        button7.isEnabled = false
+        
     }
     @IBAction func button8Tapped(_ sender: Any) {
-        print("Button 8 tapped")
+        if isXTurn {
+            button8.setBackgroundImage(#imageLiteral(resourceName: "X"), for: .normal)
+        } else {
+            button8.setBackgroundImage(#imageLiteral(resourceName: "O"), for: .normal)
+        }
+        //set whose turn it is to the opposite
+        isXTurn = !isXTurn
+        button8.isEnabled = false
+        
     }
     @IBAction func button9Tapped(_ sender: Any) {
-        print("Button 9 tapped")
+        if isXTurn {
+            button9.setBackgroundImage(#imageLiteral(resourceName: "X"), for: .normal)
+        } else {
+            button9.setBackgroundImage(#imageLiteral(resourceName: "O"), for: .normal)
+        }
+        //set whose turn it is to the opposite
+        isXTurn = !isXTurn
+        button9.isEnabled = false
+        
     }
-    
-    
-    
-    
-    
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
